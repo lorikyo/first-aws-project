13cbd7d0a08a8c5d1a0a4f3b081aa7bc8762ac7b
lorikyo <davidfan09@yahoo.com>
2020-02-19 13:23:20 +0800

    First commit

---
+++ src/test/java/com/example/firstawsproject/FirstAwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstawsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
